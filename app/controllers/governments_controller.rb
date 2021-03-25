7519e33db5b3fdc63b02c3fb242fdec78fccc478
masaki0102 <countryhouse95@gmail.com>
2021-03-26 07:11:19 +0900

    検索機能（年齢）の実装

--- app/controllers/governments_controller.rb
+++ app/controllers/governments_controller.rb
@@ -1,10 +1,24 @@
 class GovernmentsController < ApplicationController
+  before_action :search_exam, only: [:index]
+
   def index
     @governments = Government.all
+    @exams = Exam.all
+    if user_signed_in?
+      date_format = "%Y%m%d"
+      now_date = Date.today
+      @age = (now_date.strftime(date_format).to_i - current_user.birth_date.strftime(date_format).to_i )/ 10000
+    end
   end
 
   def show
     @government = Government.find(params[:id])
     @exams = @government.exams
+  end
+
+  private
+
+  def search_exam
+    @p = Exam.ransack(params[:q])  # 検索オブジェクトを生成
   end
 end
