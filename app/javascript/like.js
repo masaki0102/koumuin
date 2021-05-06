a017f24af4386661a3b59502d6888165ec0a9ad7
masaki0102 <countryhouse95@gmail.com>
2021-05-06 17:38:26 +0900

    いいね数カウントの非同期通信

--- app/javascript/like.js
+++ app/javascript/like.js
@@ -1,13 +1,16 @@
 function like() {
-  const likes = document.querySelectorAll(".fa-star");
+  // クラス名から要素を取得
+  const likes = document.querySelectorAll(".like");
   likes.forEach(function(like){
+    //イベント発火の重複を回避
     if (like.getAttribute("data-load") != null) {
       return null;
     }
     like.setAttribute("data-load", "true");
+    // クリックしてイベント発火
     like.addEventListener("click",()=>{
-      // ここにクリックした時の処理
       const likeId = like.getAttribute("data-id");
+      const likeCount = countToNumber(like);
       const XHR = new XMLHttpRequest();
       // リクエストを指定
       XHR.open("GET", `/exams/${likeId}/likes`, true);
@@ -15,21 +18,35 @@ function like() {
       XHR.send();
       // レスポンスがあった場合の処理
       XHR.onload = () => {
+        // レスポンスのHTTPステータスを解析し、該当するエラーメッセージをアラートで表示
         if (XHR.status != 200) {
           alert(`Error ${XHR.status}: ${XHR.statusText}`);
           return null;          
         }
+        // レスポンスされたデータを変数itemに代入
         const item = XHR.response.like;
         if (item) {
-          like.setAttribute( "style", "color:gold" ); 
+          like.setAttribute("class","fas fa-star");
+          like.setAttribute( "style", "color:gold" );
+          like.innerHTML =  likeCount + 1;
         }  else{
-          like.removeAttribute("style")
+          like.setAttribute("class","far fa-star");
+          like.removeAttribute("style");
+          like.innerHTML =  likeCount - 1;
          }
       };
     });
    
   });
 }
-  setInterval(like, 1000);
-// window.addEventListener("load", like);
+
+// お気に入りカウントの取得と数値への変換
+function countToNumber(count){
+   var countNumber = count.innerHTML;
+   countNumber = Number(countNumber);
+   return countNumber;
+}
+
+setInterval(like, 1000);
+
 
